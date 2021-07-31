5adee17736e1ddff79143a81a22e110b4e6bb22d
Utsaviranjan <75804499+Utsaviranjan@users.noreply.github.com>
2021-07-31 20:48:14 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,8 @@
+<?php
+  $firstname = $_POST[$firstname]
+  $lastname = $_POST[$lastname]
+  $gmail = $_POST[$gmail]
+  $subject = $_POST[$subject]
+  $message = $_POST[$message]
+  $query = $_POST[$query]
+?>
